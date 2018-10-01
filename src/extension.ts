aba26edbd2bfe32ffbd967f9799d69cefe3b4b38
Michell Stuttgart <michellstut@gmail.com>
2018-10-01 08:34:39 -0300

    [IMP] Change command name to 'odooScaffold'

--- src/extension.ts
+++ src/extension.ts
@@ -8,55 +8,61 @@ import * as path from 'path';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    let disposable = vscode.commands.registerCommand('extension.generateOdooModule', (fileObj) => {
+    let disposable = vscode.commands.registerCommand('extension.odooScaffold', (fileObj) => {
 
         // Get base path
         let basePath = fileObj.path;
 
-        const paths = [];
+        // Paths list
+        const folders = [];
 
+        // Odoo module folders
         const moduleFolder = path.join(basePath, 'new_module');
         const modelsFolder = path.join(moduleFolder, 'models');
         const testsFolder = path.join(moduleFolder, 'tests');
         const viewsFolder = path.join(moduleFolder, 'views');
         const staticFolder = path.join(moduleFolder, 'static');
 
-        paths.push(moduleFolder);
-        paths.push(modelsFolder);
-        paths.push(testsFolder);
-        paths.push(viewsFolder);
-        paths.push(staticFolder);
+        folders.push(moduleFolder);
+        folders.push(modelsFolder);
+        folders.push(testsFolder);
+        folders.push(viewsFolder);
+        folders.push(staticFolder);
 
-        for (const p of paths) {
-            fs.mkdirSync(p);
+        // Create folders
+        for (const folder of folders) {
+            fs.mkdirSync(folder);
         }
 
+        // Files list
         const files = [];
 
-        const module_manifest_file = path.join(moduleFolder, '__manifest__.py');
-        const module_init_file = path.join(moduleFolder, '__init__.py');
+        // Set the path of files
+        const moduleManifestFile = path.join(moduleFolder, '__manifest__.py');
+        const moduleInitFile = path.join(moduleFolder, '__init__.py');
 
-        const models_init_file = path.join(modelsFolder, '__init__.py');
-        const models_model_file = path.join(modelsFolder, 'model.py');
+        const modelsInitFile = path.join(modelsFolder, '__init__.py');
+        const modelsModelFile = path.join(modelsFolder, 'model.py');
 
-        const tests_init_file = path.join(testsFolder, '__init__.py');
-        const tests_model_file = path.join(testsFolder, 'test_model.py');
+        const testsInitFile = path.join(testsFolder, '__init__.py');
+        const testsModelFile = path.join(testsFolder, 'test_model.py');
 
-        const views_model_file = path.join(viewsFolder, 'model.xml');
-        const static_index_file = path.join(staticFolder, 'index.html');
+        const viewsModelFile = path.join(viewsFolder, 'model.xml');
+        const staticIndexFile = path.join(staticFolder, 'index.html');
 
-        files.push(module_manifest_file);
-        files.push(module_init_file);
+        files.push(moduleManifestFile);
+        files.push(moduleInitFile);
 
-        files.push(models_init_file);
-        files.push(models_model_file);
+        files.push(modelsInitFile);
+        files.push(modelsModelFile);
 
-        files.push(tests_init_file);
-        files.push(tests_model_file);
+        files.push(testsInitFile);
+        files.push(testsModelFile);
 
-        files.push(views_model_file);
-        files.push(static_index_file);
+        files.push(viewsModelFile);
+        files.push(staticIndexFile);
 
+        // Create the Odoo module files
         for (const f of files) {    
             if (!fs.existsSync(f)) {
                 fs.closeSync(fs.openSync(f, 'w'));
